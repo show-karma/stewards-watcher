558d1b2759862adde36d877c8a51bfacc9875e79
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2022-09-06 11:55:39 -0300

    fix: errors on eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
-import { Flex } from '@chakra-ui/react';
 import { HomeContainer } from 'containers';
 import type { NextPage } from 'next';
 
-const Home: NextPage = () => {
-  return <HomeContainer />;
-};
+const Home: NextPage = () => <HomeContainer />;
 
 export default Home;
