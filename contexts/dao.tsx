f173a89e090f43f61343d7702a4470febe98b6e4
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2022-11-25 13:50:52 -0300

    fix: problem with find not having social links

--- contexts/dao.tsx
+++ contexts/dao.tsx
@@ -1,11 +1,13 @@
 import { usePicasso } from 'hooks';
 import React, { useContext, createContext, useMemo, useState } from 'react';
 import { supportedDAOs } from 'resources';
-import { IDAOInfo, IDAOTheme } from 'types';
+import { IDAOData, IDAOInfo, IDAOTheme } from 'types';
+import { axiosInstance } from 'utils';
 
 interface IDAOProps {
   daoInfo: IDAOInfo;
   theme: IDAOTheme;
+  daoData: IDAOData | undefined;
 }
 
 export const DAOContext = createContext({} as IDAOProps);
@@ -20,6 +22,7 @@ export const DAOProvider: React.FC<ProviderProps> = ({
   selectedDAO,
 }) => {
   const [daoInfo, setDAOInfo] = useState<IDAOInfo>({} as IDAOInfo);
+  const [daoData, setDAOData] = useState<IDAOData>();
   const theme = usePicasso({ light: daoInfo.light, dark: daoInfo.dark });
 
   const searchConfig = (dao: string) => {
@@ -27,6 +30,17 @@ export const DAOProvider: React.FC<ProviderProps> = ({
     if (!findDAO) setDAOInfo({} as IDAOInfo);
     setDAOInfo(findDAO);
   };
+
+  const setupDaoInfo = async () => {
+    const { config } = daoInfo;
+    await axiosInstance
+      .get(`/dao/delegates?name=${config.DAO_KARMA_ID}&pageSize=10&offset=0`)
+      .then(res => setDAOData(res.data.data));
+  };
+
+  useMemo(() => {
+    if (daoInfo?.config?.DAO_KARMA_ID) setupDaoInfo();
+  }, [daoInfo]);
 
   useMemo(() => {
     searchConfig(selectedDAO);
@@ -36,8 +50,9 @@ export const DAOProvider: React.FC<ProviderProps> = ({
     () => ({
       daoInfo,
       theme,
+      daoData,
     }),
-    [daoInfo, theme]
+    [daoInfo, theme, daoData]
   );
 
   return (
