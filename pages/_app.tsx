234a6e634f494e6791d38e6ce4a000dbd504b402
mury12 <jamesandre79@hotmail.com>
2022-12-05 18:32:52 -0300

    feat: analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,36 @@ import type { AppProps } from 'next/app';
 import { ColorHandler } from 'contexts';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '@rainbow-me/rainbowkit/styles.css';
+import Script from 'next/script';
 
 const queryClient = new QueryClient();
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <QueryClientProvider client={queryClient}>
-    <ColorHandler cookies={pageProps.cookies}>
-      <Component {...pageProps} />
-    </ColorHandler>
-  </QueryClientProvider>
+  <>
+    <QueryClientProvider client={queryClient}>
+      <ColorHandler cookies={pageProps.cookies}>
+        <Component {...pageProps} />
+      </ColorHandler>
+    </QueryClientProvider>
+
+    {process.env.NODE_ENV === 'production' && (
+      <>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-67LDHT697P"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+
+                  gtag('config', 'G-67LDHT697P');
+                `}
+        </Script>
+      </>
+    )}
+  </>
 );
 
 export default MyApp;
