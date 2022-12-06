0796da789fc9d6c05c019f8f342c3235267b6b7b
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2022-12-06 18:40:32 -0300

    feat: set enabled in production in sentry config

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -5,6 +5,7 @@
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
+  enabled: process.env.NODE_ENV === 'production',
   dsn: 'https://34603b8b014c4e08a952edd25f6acf37@o1174022.ingest.sentry.io/4504255615205376',
   tracesSampleRate: 1.0,
 });
