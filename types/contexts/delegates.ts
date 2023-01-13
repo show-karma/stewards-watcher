b922814d867126ad7ddfcb644240a1a988609311
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2023-01-13 16:34:27 -0300

    feat: workstream filter

--- types/contexts/delegates.ts
+++ types/contexts/delegates.ts
@@ -12,3 +12,19 @@ export interface IVoteInfo {
 }
 
 export type IStatusOptions = 'active' | 'inactive' | 'withdrawn' | 'recognized';
+
+export type IWorkstream = {
+  id: number;
+  userId: number;
+  name: string;
+  daoName: string;
+  description: string;
+  treasuryBalance: number;
+  interestForm: string;
+  score: number;
+  discord: string;
+  daoDocument: string;
+  createdAt: string;
+  updatedAt: string;
+  email: string;
+};
