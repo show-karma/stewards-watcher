ccb98d345533936a35085c709348287ea167d30a
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2023-01-13 18:31:48 -0300

    fix: fixing period condition to show

--- components/Filters/OrderByFilters.tsx
+++ components/Filters/OrderByFilters.tsx
@@ -10,24 +10,21 @@ import { StatFilter } from './StatFilter';
 export const OrderByFilters = () => {
   const { interests } = useDelegates();
   const { theme, daoInfo } = useDAO();
-  const haveFilter =
-    interests.length > 0 || daoInfo.config.DAO_DEFAULT_SETTINGS?.STATUS_FILTER;
+
   return (
     <Flex gap="4" align="end" flexDir={['row']} flexWrap="wrap">
-      {haveFilter && (
-        <Flex flexDir="column" w={{ base: 'full', md: 'max-content' }}>
-          <Text fontFamily="heading" color={theme.filters.head}>
-            Filter by
-          </Text>
-          <Flex flexDir="row" gap="4" flexWrap="wrap">
-            {interests.length > 0 && <InterestsFilter />}
-            {daoInfo.config.DAO_DEFAULT_SETTINGS?.STATUS_FILTER && (
-              <DelegateStatusFilter />
-            )}
-            <PeriodFilter />
-          </Flex>
+      <Flex flexDir="column" w={{ base: 'full', md: 'max-content' }}>
+        <Text fontFamily="heading" color={theme.filters.head}>
+          Filter by
+        </Text>
+        <Flex flexDir="row" gap="4" flexWrap="wrap">
+          {interests.length > 0 && <InterestsFilter />}
+          {daoInfo.config.DAO_DEFAULT_SETTINGS?.STATUS_FILTER && (
+            <DelegateStatusFilter />
+          )}
+          <PeriodFilter />
         </Flex>
-      )}
+      </Flex>
     </Flex>
   );
 };
