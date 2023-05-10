a117bf9c6dbb840a468897c19a4ca8eb91343f7b
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2023-05-10 18:05:56 -0300

    feat: adding small cases to format

--- utils/formatNumber.ts
+++ utils/formatNumber.ts
@@ -1,6 +1,22 @@
 import humanFormat from 'human-format';
+import numbro from 'numbro';
 
 export const formatNumber = (numberToFormat: number | string) => {
+  if (numberToFormat === '-') return '-';
+  if (numberToFormat === '0' || +numberToFormat === 0) return '0';
+  if (+numberToFormat <= 1) {
+    const formattedNumber = numbro(numberToFormat).format({
+      mantissa: 2,
+      trimMantissa: true,
+    });
+    if (formattedNumber === 'NaN' || formattedNumber === '0') {
+      return '< 0.01';
+    }
+    return numbro(numberToFormat).format({
+      mantissa: 2,
+      trimMantissa: true,
+    });
+  }
   if (typeof numberToFormat === 'string') {
     return humanFormat(+numberToFormat, {
       maxDecimals: 2,
