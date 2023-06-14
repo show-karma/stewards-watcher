7c95568dc3290dd68aeaf23947047d14d645362f
mury12 <jamesandre79@hotmail.com>
2023-06-14 18:12:39 -0300

    feat: get proposal names from polkassembly

---
+++ utils/moonbeam/polkassembly.ts
@@ -0,0 +1,62 @@
+import axios, { AxiosInstance } from 'axios';
+import { NumberIsh } from 'types';
+
+export interface Post {
+  comments_count: number;
+  created_at: Date;
+  curator: string;
+  description: string;
+  end?: Date;
+  hash: string;
+  parent_bounty_index: null;
+  post_id: number;
+  status: string;
+  title: string;
+}
+
+interface OnChainPostsRes {
+  count: number;
+  posts: Post[];
+}
+
+const routes = {
+  onChainPosts: (trackNo: NumberIsh, page = 1, limit = 100) => ({
+    url: '/listing/on-chain-posts',
+    params: {
+      page,
+      proposalType: 'referendums_v2',
+      listingLimit: limit,
+      trackNo,
+      trackStatus: 'All',
+      sortBy: 'newest',
+    },
+  }),
+};
+
+class PolkassemblyClient {
+  private api: AxiosInstance;
+
+  constructor() {
+    this.api = axios.create({
+      baseURL: 'https://api.polkassembly.io/api/v1',
+    });
+  }
+
+  async fetchOnChainPosts(
+    trackNo: NumberIsh,
+    network: 'moonriver' | 'moonbeam' = 'moonriver',
+    page = 1,
+    limit = 100
+  ): Promise<Post[]> {
+    const { url, params } = routes.onChainPosts(trackNo, page, limit);
+    const { data } = await this.api.get<OnChainPostsRes>(url, {
+      params,
+      headers: { 'X-Network': network },
+    });
+    return data.posts;
+  }
+}
+
+const polkassembly = new PolkassemblyClient();
+
+export { polkassembly, PolkassemblyClient };
