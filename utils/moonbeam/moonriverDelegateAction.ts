f36420cfadefd3e95bd2d3ac1d18bcdc6ba67144
Mahesh Murthy <mahesh@mmurthy.com>
2023-06-20 22:38:35 -0700

    Convert gas to BigInt

--- utils/moonbeam/moonriverDelegateAction.ts
+++ utils/moonbeam/moonriverDelegateAction.ts
@@ -46,7 +46,7 @@ export const moonriverDelegateAction =
       address: batchContractAddr,
       abi: batchContractAbi,
       functionName: 'batchAll',
-      gas: 11250000,
+      gas: BigInt(11250000),
       args,
     });
 
