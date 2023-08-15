05ccbddf0515318fc955fa2a528b6af6fb4d75cf
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2023-08-15 17:51:10 -0300

    feat: adding moonbeam custom network and rpc

--- utils/moonbeam/network.ts
+++ utils/moonbeam/network.ts
@@ -1,37 +1,37 @@
-export declare const moonbeam: {
-  readonly id: 1284;
-  readonly name: 'Moonbeam';
-  readonly network: 'moonbeam';
-  readonly nativeCurrency: {
-    readonly decimals: 18;
-    readonly name: 'GLMR';
-    readonly symbol: 'GLMR';
-  };
-  readonly rpcUrls: {
-    readonly public: {
-      readonly http: readonly ['https://rpc.api.moonbeam.network'];
-      readonly webSocket: readonly ['wss://wss.api.moonbeam.network'];
-    };
-    readonly default: {
-      readonly http: readonly ['https://rpc.api.moonbeam.network'];
-      readonly webSocket: readonly ['wss://wss.api.moonbeam.network'];
-    };
-  };
-  readonly blockExplorers: {
-    readonly default: {
-      readonly name: 'Moonscan';
-      readonly url: 'https://moonscan.io';
-    };
-    readonly etherscan: {
-      readonly name: 'Moonscan';
-      readonly url: 'https://moonscan.io';
-    };
-  };
-  readonly contracts: {
-    readonly multicall3: {
-      readonly address: '0xcA11bde05977b3631167028862bE2a173976CA11';
-      readonly blockCreated: 609002;
-    };
-  };
-  readonly testnet: false;
+export const moonbeam = {
+  id: 1284,
+  name: 'Moonbeam',
+  network: 'moonbeam',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'GLMR',
+    symbol: 'GLMR',
+  },
+  rpcUrls: {
+    public: {
+      http: ['https://rpc.api.moonbeam.network'],
+      webSocket: ['wss://wss.api.moonbeam.network'],
+    },
+    default: {
+      http: ['https://rpc.api.moonbeam.network'],
+      webSocket: ['wss://wss.api.moonbeam.network'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Moonscan',
+      url: 'https://moonscan.io',
+    },
+    etherscan: {
+      name: 'Moonscan',
+      url: 'https://moonscan.io',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11' as `0x${string}`,
+      blockCreated: 609002,
+    },
+  },
+  testnet: false,
 };
