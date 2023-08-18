042b4a75424e34f246579de6b6f7b71c8c283083
mury12 <jamesandre79@hotmail.com>
2023-08-18 19:11:40 -0300

    fix: moonbeam proposals

--- utils/api/proposals.ts
+++ utils/api/proposals.ts
@@ -2,7 +2,9 @@ import { MoonbeamProposal, NumberIsh } from 'types';
 import { MoonbeamWSC } from '../moonbeam/moonbeamwsc';
 import { polkassembly, Post } from '../moonbeam/polkassembly';
 
-export const moonriverProposals = async () => {
+export const moonriverProposals = async (
+  daoName: 'moonbeam' | 'moonriver' = 'moonriver'
+) => {
   const client = await MoonbeamWSC.createClient();
 
   const proposals = await client.getProposals();
@@ -10,7 +12,7 @@ export const moonriverProposals = async () => {
   const postWithTrackId: (Post & { trackId: NumberIsh })[] = [];
 
   const promises = tracks.map(async track => {
-    const posts = await polkassembly.fetchOnChainPosts(track.id, 'moonriver');
+    const posts = await polkassembly.fetchOnChainPosts(track.id, daoName);
     postWithTrackId.push(
       ...posts.map(post => ({ ...post, trackId: track.id }))
     );
@@ -39,39 +41,4 @@ export const moonriverProposals = async () => {
   return result;
 };
 
-export const moonbeamProposals = async () => {
-  const client = await MoonbeamWSC.createClient();
-
-  const proposals = await client.getProposals();
-  const tracks = client.getTracks(true);
-  const postWithTrackId: (Post & { trackId: NumberIsh })[] = [];
-
-  const promises = tracks.map(async track => {
-    const posts = await polkassembly.fetchOnChainPosts(track.id, 'moonbeam');
-    postWithTrackId.push(
-      ...posts.map(post => ({ ...post, trackId: track.id }))
-    );
-  });
-
-  await Promise.all(promises);
-
-  const result: (MoonbeamProposal & {
-    proposal: string;
-    trackId: NumberIsh;
-  })[] = [];
-
-  postWithTrackId.forEach(post => {
-    const currentProposal = proposals.find(
-      proposal => +proposal.proposalId === +post.post_id
-    );
-
-    if (currentProposal) {
-      result.push({
-        ...currentProposal,
-        proposal: post.title,
-        trackId: post.trackId,
-      });
-    }
-  });
-  return result;
-};
+export const moonbeamProposals = () => moonriverProposals('moonbeam');
