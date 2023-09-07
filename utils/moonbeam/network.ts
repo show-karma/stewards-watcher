11e51a10fe8db50e949d8de5ea090f563adb88e1
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2023-09-07 11:42:24 -0300

    feat: moonbase network

--- utils/moonbeam/network.ts
+++ utils/moonbeam/network.ts
@@ -35,3 +35,41 @@ export const moonbeam = {
   },
   testnet: false,
 };
+
+export const moonbase = {
+  id: 1287,
+  name: 'Moonbase',
+  network: 'moonbase',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'DEV',
+    symbol: 'DEV',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.api.moonbase.moonbeam.network'],
+      webSocket: ['wss://wss.api.moonbase.moonbeam.network'],
+    },
+    public: {
+      http: ['https://rpc.api.moonbase.moonbeam.network'],
+      webSocket: ['wss://wss.api.moonbase.moonbeam.network'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Moonscan',
+      url: 'https://moonbase.moonscan.io',
+    },
+    etherscan: {
+      name: 'Moonscan',
+      url: 'https://moonbase.moonscan.io',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11' as `0x${string}`,
+      blockCreated: 1850686,
+    },
+  },
+  testnet: true,
+};
