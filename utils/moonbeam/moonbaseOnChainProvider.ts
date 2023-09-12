7f8a002eb6ee500ead8782222ada7878d1fc0ae6
mury12 <jamesandre79@hotmail.com>
2023-09-12 20:36:45 -0300

    fix: api/proposals call

--- utils/moonbeam/moonbaseOnChainProvider.ts
+++ utils/moonbeam/moonbaseOnChainProvider.ts
@@ -87,7 +87,9 @@ function concatOnChainProposals(proposals: any[], votes: any[]) {
 async function proposalsWithMetadata(): Promise<
   (MoonbeamProposal & { proposal: string; trackId: NumberIsh })[]
 > {
-  const { data } = await axios.get('/api/proposals?dao=moonbase');
+  const { data } = await axios.get(
+    '/api/proposals?dao=moonbase&source=on-chain'
+  );
   return data;
 }
 
