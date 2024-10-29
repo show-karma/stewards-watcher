fff1f8c6025bdb95ad5171bcaf920451c7298e97
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2024-10-29 15:50:18 -0300

    feat: auth guard

---
+++ components/pages/delegate-compensation/Admin/AdminGuard/index.tsx
@@ -0,0 +1,41 @@
+import { Flex, Text } from '@chakra-ui/react';
+import { useAuth, useWallet } from 'contexts';
+import { FC, ReactNode } from 'react';
+
+interface AdminGuardProps {
+  children: ReactNode;
+}
+
+export const AdminGuard: FC<AdminGuardProps> = ({ children }) => {
+  const { isConnected } = useWallet();
+  const { isAuthenticated, isDaoAdmin } = useAuth();
+  if (!isAuthenticated || !isConnected) {
+    return (
+      <Flex
+        flexDir="column"
+        alignItems="center"
+        justifyContent="center"
+        py="10"
+        h="full"
+        w="full"
+      >
+        <Text>You must be logged in to access this page</Text>
+      </Flex>
+    );
+  }
+  if (!isDaoAdmin) {
+    return (
+      <Flex
+        flexDir="column"
+        alignItems="center"
+        justifyContent="center"
+        py="10"
+        h="full"
+        w="full"
+      >
+        <Text>You are not authorized to access this page</Text>;
+      </Flex>
+    );
+  }
+  return <>{children}</>;
+};
--- layouts/delegateCompensationAdmin.tsx
+++ layouts/delegateCompensationAdmin.tsx
@@ -1,72 +1,62 @@
 import { Flex, Heading } from '@chakra-ui/react';
 import { ChakraLink } from 'components';
+import { AdminGuard } from 'components/pages/delegate-compensation/Admin/AdminGuard';
 import { MonthDropdown } from 'components/pages/delegate-compensation/MonthDropdown';
-import { useAuth, useDAO, useWallet } from 'contexts';
-import { useRouter } from 'next/router';
+import { useDAO } from 'contexts';
 
 export const DelegateCompensationAdminLayout = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
-  const { isDaoAdmin } = useAuth();
-  const { isConnected } = useWallet();
   const { theme } = useDAO();
-  const router = useRouter();
-  if (!isDaoAdmin) {
-    if (isConnected) {
-      router.push('/delegate-compensation');
-    }
-    return (
-      <Flex py="10">
-        <p>You are not authorized to access this page</p>
-      </Flex>
-    );
-  }
+
   return (
-    <Flex flexDir="column" w="full" p={5}>
-      <Heading mb={10} color={theme.text}>
-        Admin Dashboard
-      </Heading>
-      <Flex
-        flexDirection={['column', 'row']}
-        justifyContent="flex-start"
-        gap="10"
-      >
-        <Flex flexDirection="column" gap="4" maxW="200px" w="full">
-          <MonthDropdown />
-          <Flex
-            flexDir="column"
-            gap={2}
-            px="2"
-            py="3"
-            bgColor={theme.card.background}
-            rounded="lg"
-          >
-            <ChakraLink
-              px="4"
+    <AdminGuard>
+      <Flex flexDir="column" w="full" p={5}>
+        <Heading mb={10} color={theme.text}>
+          Admin Dashboard
+        </Heading>
+        <Flex
+          flexDirection={['column', 'row']}
+          justifyContent="flex-start"
+          gap="10"
+        >
+          <Flex flexDirection="column" gap="4" maxW="200px" w="full">
+            <MonthDropdown />
+            <Flex
+              flexDir="column"
+              gap={2}
+              px="2"
               py="3"
-              bgColor={theme.card.statBg}
-              href="/delegate-compensation/admin"
+              bgColor={theme.card.background}
               rounded="lg"
-              color={theme.text}
             >
-              Proposals
-            </ChakraLink>
-            <ChakraLink
-              px="4"
-              py="3"
-              bgColor={theme.card.statBg}
-              rounded="lg"
-              href="/delegate-compensation/admin/delegate"
-              color={theme.text}
-            >
-              Delegates
-            </ChakraLink>
+              <ChakraLink
+                px="4"
+                py="3"
+                bgColor={theme.card.statBg}
+                href="/delegate-compensation/admin"
+                rounded="lg"
+                color={theme.text}
+              >
+                Proposals
+              </ChakraLink>
+              <ChakraLink
+                px="4"
+                py="3"
+                bgColor={theme.card.statBg}
+                rounded="lg"
+                href="/delegate-compensation/admin/delegate"
+                color={theme.text}
+              >
+                Delegates
+              </ChakraLink>
+            </Flex>
           </Flex>
+          {children}
         </Flex>
-        {children}
       </Flex>
-    </Flex>
+    </AdminGuard>
   );
 };
