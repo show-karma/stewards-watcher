6cbd7f8b325adc15ef4e118051f013bc78670885
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2024-11-18 12:06:23 -0300

    feat: adding content policy

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 // const { withSentryConfig } = require('@sentry/nextjs');
 const removeImports = require('next-remove-imports')();
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
+
 const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
@@ -10,14 +23,35 @@ const nextConfig = {
   //   hideSourceMaps: true,
   // },
   async headers() {
-    return [{
-      source: "/api/:path*",
-      headers: [
-        { key: "Access-Control-Allow-Origin", value: "*" },
-        { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-        { key: "Access-Control-Allow-Headers", value: "X-Requested-With, Content-Type, Authorization" },
-      ],
-    }]
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Content-Type, Authorization',
+          },
+        ],
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ];
   },
   webpack(config, { nextRuntime }) {
     // as of Next.js latest versions, the nextRuntime is preferred over `isServer`, because of edge-runtime
