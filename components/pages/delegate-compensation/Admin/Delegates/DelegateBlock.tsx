368b7a14a89a7e15e78a251d58e4b64504bc4f47
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2025-01-31 11:50:00 -0300

    feat: adding copy button to address

--- components/pages/delegate-compensation/Admin/Delegates/DelegateBlock.tsx
+++ components/pages/delegate-compensation/Admin/Delegates/DelegateBlock.tsx
@@ -1,14 +1,29 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, Icon, Text, useClipboard } from '@chakra-ui/react';
 import { blo } from 'blo';
 import { CheckIcon } from 'components/Icons/CheckIcon';
 import { ImgWithFallback } from 'components/ImgWithFallback';
 import { useDAO } from 'contexts';
 import { useDelegateCompensation } from 'contexts/delegateCompensation';
+import { useToasty } from 'hooks';
+import { IoCopy } from 'react-icons/io5';
 import { truncateAddress } from 'utils';
 
 export const DelegateBlock = () => {
   const { delegateInfo } = useDelegateCompensation();
   const { theme } = useDAO();
+  const { onCopy } = useClipboard(delegateInfo?.publicAddress || '');
+  const { toast } = useToasty();
+
+  const handleCopy = () => {
+    onCopy();
+    toast({
+      title: 'Address copied successfully',
+      status: 'success',
+      duration: 2000,
+      isClosable: true,
+    });
+  };
+
   return (
     <Flex flexDir="row" gap="5" align="center">
       <Flex position="relative">
@@ -40,13 +55,22 @@ export const DelegateBlock = () => {
             delegateInfo?.ensName ||
             delegateInfo?.publicAddress}
         </Text>
-        <Text
-          color={theme.compensation?.card.text}
-          fontWeight={400}
-          fontSize="14px"
-        >
-          {truncateAddress(delegateInfo?.publicAddress as string)}
-        </Text>
+        <Flex align="center" gap="2">
+          <Text
+            color={theme.compensation?.card.text}
+            fontWeight={400}
+            fontSize="14px"
+          >
+            {truncateAddress(delegateInfo?.publicAddress as string)}
+          </Text>
+          <Icon
+            as={IoCopy}
+            color={theme.compensation?.card.text}
+            boxSize="4"
+            cursor="pointer"
+            onClick={handleCopy}
+          />
+        </Flex>
       </Flex>
     </Flex>
   );
